d0c1257de199b9e576c4d35fda4b4644e64f8c11
Projjal Mukherjee <projjalmukherjee@gmail.com>
2023-01-21 16:56:06 +0530

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-2 card">
+            <div class="card-header">{{ __('Sidebar') }}</div>
+           
+            <a href="/company">Company</a>
+            <br/>
+            <br/>
+            <a href="{{ route('employee.index') }}">Employee</a>
+            <br/>
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
+                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -16,9 +26,7 @@
 
                     {{ __('You are logged in!') }}
 
-                    <a href="/company">Company</a>
-
-                    <a href="{{ route('employee.index') }}">Employee</a>
+                    
                 </div>
             </div>
         </div>
